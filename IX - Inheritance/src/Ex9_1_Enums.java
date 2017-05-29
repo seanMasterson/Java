e27c7afec07fceb6f84d907c9bc1daac11bdd590
semaster@tcd.ie <dsd13@SD-13.fasfg.local>
2017-05-29 15:28:20 +0100

    "IX - Inheritance"

---
+++ IX - Inheritance/src/Ex9_1_Enums.java
@@ -0,0 +1,19 @@
+enum MaterialType 
+{
+	ALUMINIUM, GRANITE, LEATHER, PINE, ALDER, MAHOGANY, GLASS
+};
+
+enum IsExpandable 
+{
+	YES, NO
+};
+
+enum HasHeadboard 
+{
+	YES, NO
+};
+
+enum HasGasLift 
+{
+	YES, NO
+};

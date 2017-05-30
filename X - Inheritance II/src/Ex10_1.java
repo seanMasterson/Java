4a7a6b346cb0c61a7c41b26f3eea02c4513f2b5c
semaster@tcd.ie <dsd13@SD-13.fasfg.local>
2017-05-30 10:57:48 +0100

    "X- Inheritance II"

--- X - Inheritance II/src/Ex10_1.java
+++ X - Inheritance II/src/Ex10_1.java
@@ -2,12 +2,16 @@ public class Ex10_1
 {
 	public static void main(String[] args) 
 	{
-		Shape[] shapeArray = new Shape[4];
+		Shape[] shapeArray = new Shape[8];
 		
 		shapeArray[0] = new Rectangle(4,5, Shape.Colours.BLACK);
 		shapeArray[1] = new Triangle(Shape.Colours.PINK);
 		shapeArray[2] = new Circle(6, Shape.Colours.GREEN);
-		shapeArray[3] = new Shape();
+		shapeArray[3] = new Shape(Shape.Colours.ORANGE);
+		shapeArray[4] = new Rectangle(3,7, Shape.Colours.YELLOW);
+		shapeArray[5] = new Triangle(Shape.Colours.BLUE);
+		shapeArray[6] = new Circle(8, Shape.Colours.RED);
+		shapeArray[7] = new Shape(Shape.Colours.WHITE);
 		
 		for(Shape shape : shapeArray) 
 		{

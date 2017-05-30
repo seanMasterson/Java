5b5ae8e40b8faa17f2485315f50832081dad9adb
semaster@tcd.ie <dsd13@SD-13.fasfg.local>
2017-05-30 09:33:59 +0100

    "XI - Inheritance III"

--- XI - Inheritance III [Payroll]/src/Ex11_1.java
+++ XI - Inheritance III [Payroll]/src/Ex11_1.java
@@ -5,7 +5,7 @@ public class Ex11_1
 	public static void main(String[] args) 
 	{
 		Boss b0 = new Boss("Death Spell","Omega", LocalDate.parse("1992-10-05"), 13.49);
-		CommissionWorker c0 = new CommissionWorker("Le Vie est Prelude","Le Mort Orgasme", LocalDate.parse("1995-06-20"), 30.55, 12.56, 5);
+		CommissionWorker c0 = new CommissionWorker("Para","Celsus", LocalDate.parse("1995-06-20"), 30.55, 12.56, 5);
 		HourlyWorker h0 = new HourlyWorker("Luc","Lemay", LocalDate.parse("1876-08-11"), 134.70, 4);
 		PieceWorker p0 = new PieceWorker("Scott","Clendenin", LocalDate.parse("1990-12-15"), 3, 4.6);
 		

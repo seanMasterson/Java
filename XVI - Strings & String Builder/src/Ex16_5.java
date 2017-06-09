d015bb0b37bc2376b55d2989b04d1b7df9674441
semaster@tcd.ie <dsd13@fasfg.local>
2017-06-09 12:37:39 +0100

    XVI - Strings

--- XVI - Strings & String Builder/src/Ex16_5.java
+++ XVI - Strings & String Builder/src/Ex16_5.java
@@ -5,20 +5,19 @@ public class Ex16_5
 	public static void main(String[] args) 
 	{
 		Scanner sc0 = new Scanner(System.in);
-		String[] word = new String[1];
 		
 		System.out.println("Enter a word: ");
-		String word0 = sc0.nextLine();
-		
-		word[0] = word0;
+		String word = sc0.nextLine();
 		
 		System.out.println("Enter a search character: ");
 		char character = sc0.next().charAt(0);
 		
-		for(String c : word) 
+		for(int i = 0; i < word.length(); i++) 
 		{
-			if(c.equals(character))
-				System.out.println( c + " was found at position " + word[character]);
+			if(word.charAt(i) == character)
+				System.out.println( character + " was found at position: " + i);
 		}
+		
+		sc0.close();
 	}
 }
